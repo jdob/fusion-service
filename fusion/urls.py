9771283c1c980f88cd696b68f2266c3eceb1c436
Shivam Chamoli <dhruvchamoli@gmail.com>
2018-07-23 16:21:59 -0400

    urls for links

--- fusion/urls.py
+++ fusion/urls.py
@@ -37,11 +37,16 @@ comments_view = PartnerViewSet.as_view(comments_mapping)
 contacts_mapping = {'delete': 'contacts', 'patch': 'contacts'}
 contacts_view = PartnerViewSet.as_view(contacts_mapping)
 
+links_mapping = {'delete': 'links', 'patch': 'links'}
+links_view = PartnerViewSet.as_view(links_mapping)
+
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^partners/(?P<pk>[^/.]+)/engagements/(?P<engagement_id>[^/.]+)/$', engagements_view),
     url(r'^partners/(?P<pk>[^/.]+)/comments/(?P<comment_id>[^/.]+)/$', comments_view),
     url(r'^partners/(?P<pk>[^/.]+)/contacts/(?P<contact_id>[^/.]+)/$', contacts_view),
+    url(r'^partners/(?P<pk>[^/.]+)/links/(?P<link_id>[^/.]+)/$', 
+        links_view),
     path('admin/', admin.site.urls),
     url(r'^api-token-auth/', views.obtain_auth_token),
 ]
