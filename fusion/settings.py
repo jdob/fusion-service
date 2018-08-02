41591c6d0e4eff6eaf9bd921fdb4a8867e9a85d1
Jay Dobies <jason.dobies@redhat.com>
2018-08-02 11:02:14 -0400

    Alphabetize installed apps

--- fusion/settings.py
+++ fusion/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'fusion.service',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
